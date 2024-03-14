a0fd6fb642a609f72d4ec8225f267746e8feab4e
navv4 <160163787+navv4@users.noreply.github.com>
2024-03-14 12:02:42 -0400

    Add files via upload

--- javascript/js.js
+++ javascript/js.js
@@ -11,3 +11,12 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+  var videos = ["https://www.youtube.com/embed/jKh-DP89FPY?si=EXBGeuu6REVFXrUf", "https://www.youtube.com/embed/y3pJOimnOy8?si=au_23XuWoCAWraJJ","https://www.youtube.com/embed/GaLV82vOSsQ?si=VZ2-UuU5hYZeqYdW","https://www.youtube.com/embed/IDYxLj3bYds?si=xM0FGRNKQLIhMVSL", "https://www.youtube.com/embed/pu54_7M35Vs?si=1mRpTwS6nR5zICay"];
+
+
+  var rand = Math.floor(Math.random() * 5)
+  console.log("random number " + rand);
+
+  $('#youtubevideo').attr("src", videos[rand]);
+
